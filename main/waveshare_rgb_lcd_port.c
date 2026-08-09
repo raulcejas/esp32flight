60dab85c9d701502891b416021981f673f267ec9
raulcejas <34383101+raulcejas@users.noreply.github.com>
2026-08-09 11:47:27 -0400

    Update waveshare_rgb_lcd_port.c

--- main/waveshare_rgb_lcd_port.c
+++ main/waveshare_rgb_lcd_port.c
@@ -407,7 +407,7 @@ esp_err_t waveshare_esp32_s3_rgb_lcd_init(void)
     esp_lcd_panel_io_handle_t tp_io_handle = NULL;
     esp_lcd_panel_io_i2c_config_t tp_io_config = ESP_LCD_TOUCH_IO_I2C_GT911_CONFIG();
     /* Set explicitly for ESP-IDF v5.2+ panel IO driver */
-    tp_io_config.scl_clock_hz = 400000;
+    tp_io_config.scl_speed_hz = 400000;
 
     esp_lcd_touch_config_t tp_cfg = {
         .x_max = EXAMPLE_LCD_H_RES,
